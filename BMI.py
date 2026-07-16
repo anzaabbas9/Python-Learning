34853b5dc2fedcdac6ace02a0fd3a762b69c3b0f
anzaabbas9 <anzaabbas9@gmail.com>
2026-07-16 20:37:49 +0500

    operaters parctice

---
+++ BMI.py
@@ -0,0 +1,4 @@
+weight=input("enter your weight in kg:")
+height=input("enter your height in meters:")
+BMI=float(weight)/float(height)**2
+print(f"your BMI is:{BMI:.2f}")
--- variable_and_types.py
+++ variable_and_types.py
@@ -1,3 +1,4 @@
+"""
 a=input("enter value of a:")
 b=input("enter value of b:")
 temp=a
@@ -10,3 +11,16 @@ age=input("what is your age?")
 print("my name is:"+ name)
 print("my age is:"+ age)
 print(name+" "+age)
+a=10
+b=2
+print(a<b)
+print(type(a<b))
+print(len(str(a)))
+a=input("enter value of a:")
+b=input("enter value of b:")
+print(a+b)
+print(int(a)+int(b))
+
+a=input("enter a two digit number:")
+print(int(a[0])+int(a[1]))"""
+print("hey anza\"how are you\"")
