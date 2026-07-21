87c27fb0f43454cd3655e1b2cb82130dcd9dcdb0
anzaabbas9 <anzaabbas9@gmail.com>
2026-07-21 11:00:10 +0500

    add hangman image in hangman game

--- hangmann_game.py
+++ hangmann_game.py
@@ -1,5 +1,62 @@
 import random
 word_list=['apple','tiger','eagle','carrot','ali']
+stages=['''
+-+----+
+  |   |
+  O   |
+ /|\\ |
+ / \\ |
+      |
+=========
+''','''
+-+----+
+  |   |
+  O   |
+ /|\\ |
+ /    |
+      |
+=========
+''','''
+-+----+
+  |   |
+  O   |
+ /|\\ |
+      |
+      |
+=========
+''','''
+-+----+
+  |   |
+  O   |
+  |\\ |
+      |
+      |
+=========
+''','''
+-+----+
+  |   |
+  O   |
+  |   |
+      |
+      |
+========
+''','''
+-+----+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''','''
+-+----+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word=random.choice(word_list)
 attempt=6
 print(list(word))
@@ -15,6 +72,7 @@ while True:
     else:
      attempt-=1
     print(" ".join(blank_list))
+    print(stages[attempt])
     if '-' not in blank_list :
         break
     if attempt<=0:
